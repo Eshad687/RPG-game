e0459440c9fd37927288a30b3265d172cc0ab833
Nazmuddin Al Masood <nazmumasood96@gmail.com>
2020-03-31 12:20:15 +0600

    first commit

---
+++ Assets/Scripts/Equipment.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "New Eqipment",menuName ="Inventory/Equipment")]
+public class Equipment : Item {
+
+    public EquipmentSlot equipSlot; // slot to store items
+    public SkinnedMeshRenderer mesh;
+    public EquipmentMeshRegion[] coveredMeshRegions;
+
+    public int armorModifier; //armor value
+    public int damageModifier; //damage value
+
+    public override void Use()
+    {
+        base.Use();
+        EquipmentManager.instance.Equip(this); //Equipm it
+        RemoveFromInventory(); //Remove it from the inventory
+    }
+
+}
+
+public enum EquipmentSlot { Head, Chest, Legs, Weapon, Shield, Feet}
+public enum EquipmentMeshRegion { Legs, Arms, Torso}; //Corresponds to body blendshapes
---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+
+    public float radius = 3f;
+    public Transform interactionTransform;
+
+    bool isFocus = false;
+    Transform player;
+
+    bool hasInteracted = false;
+
+    public virtual void Interact()
+    {
+        //Need to be overwritten
+        Debug.Log("Interacting with " + transform.name);
+    }
+
+    private void Update()
+    {
+        if (isFocus && !hasInteracted)
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if(distance <= radius)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDeFocused()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+    }
+
+
+}
